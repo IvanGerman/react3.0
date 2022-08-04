b0a5d3520136637cf1e9ab9908ef6e6a37dff10b
IvanGerman <ivgvi@mail.ru>
2022-08-04 20:29:17 +0700

    feat: using import/export and props object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import StartPage from './StartPage.js';
 
 function App() {
   return (
     <div className="App">
       project react3.0
+      <StartPage key1="111" key2="222"/>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/StartPage.js
@@ -0,0 +1,11 @@
+const StartPage = (props) => {
+  return (
+    <div>
+      StartPage
+      <p>{props.key1}</p>
+      <p>{props.key2}</p>
+    </div>
+  )
+}
+
+export default StartPage;
