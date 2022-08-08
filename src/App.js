ef7de5ebe2498e5fcd8e1e97ff5e3e2beb11a9b4
IvanGerman <ivgvi@mail.ru>
2022-08-08 17:45:40 +0700

    feat: add BrowserRouter and Route tags for routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import {BrowserRouter, Route} from 'react-router-dom';
+
 import './App.css';
 import StartPage from './StartPage.js';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      project react3.0
-      <StartPage key1="111" key2="222"/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        project react3.0
+        <div className='routes-div'>
+          <Route path='/gamePage' component={null}></Route>
+          <Route path='/gameStats' component={null}></Route>
+        </div>
+        <StartPage key1="111" key2="222"/>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
