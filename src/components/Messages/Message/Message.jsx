d38fd12dbaf277f234f4894d4e2d110ce07025fa
IvanGerman <ivgvi@mail.ru>
2022-11-16 23:31:32 +0700

    feat: add css properties for Message components

--- src/components/Messages/Message/Message.jsx
+++ src/components/Messages/Message/Message.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import './Message.css';
 
 const Message = (props) => { 
 
+  const f1 = () => {
+    alert('f1');
+  }
+
   return (
-    <div className="message">
+    <div className="message" id={props.id} onClick={f1}>
       {props.message}
     </div>
   )
--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Message from "./Message/Message";
+import './Messages.css';
+
 
 const Messages = (props) => { console.log('props.state',props.state);
 
   let allMessages = props.state.messages.map((elem, index) => {
-    return ( <Message message={elem.message} key={index}></Message> );
+    return ( <Message id={elem.id} message={elem.message} key={index}></Message> );
   })
   return (
     <div className="messages">
