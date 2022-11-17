289225c755b5d4be7019932df489f30e4395c415
IvanGerman <ivgvi@mail.ru>
2022-11-17 18:12:06 +0700

    feat: add delete message feature

--- src/components/Messages/Message/Message.jsx
+++ src/components/Messages/Message/Message.jsx
@@ -3,8 +3,34 @@ import './Message.css';
 
 const Message = (props) => { 
 
-  const f1 = () => {
+  async function myDeleteFunc(messageID) {
+    
+
+    await fetch(`http://localhost:3001/api/messages/${messageID}`, {
+      method: 'DELETE', 
+      mode: 'cors', 
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IjExMUBtYWlsLnJ1IiwidXNlcklkIjoiNjJkYTNjOTg0NjgxMWYwZmM4OTI1ZDVlIiwiaWF0IjoxNjY4NTE4ODM2LCJleHAiOjE2Njg2OTE2MzZ9.9g4k1nbv_VZdb1y5xJH3jFRTH1tCMi3mj254qy5Vw4A'
+      }
+    })
+    .then((response) => { 
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+  };
+
+
+  const f1 = function(element) {
     alert('f1');
+    console.log(element.target.id);
+    let messageID = element.target.id;
+    //DELETE request to server
+    myDeleteFunc(messageID);
+    // dispatch call to delete it from store and rerender
+    
   }
 
   return (
