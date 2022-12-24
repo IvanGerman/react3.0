fa4f24049e7ec12c481d07fdff775b56a37398b2
IvanGerman <ivgvi@mail.ru>
2022-12-24 20:26:33 +0700

    feat: add Navlink to StatsPage divs

--- src/components/StatsPage/StatsPage.jsx
+++ src/components/StatsPage/StatsPage.jsx
@@ -1,7 +1,18 @@
+import { NavLink } from "react-router-dom";
+
 const StatsPage = () => {
   return (
     <div className="stats-page">
       StatsPage
+      <div></div>
+      <br/>
+      <NavLink to = {'/statspage/statsid11111'}>
+        <div>statsid11111</div>
+      </NavLink>
+      <br/>
+      <NavLink to = {'/statspage/statsid22222'}>
+        <div>statsid22222</div>
+      </NavLink>
     </div>
   )
 }
