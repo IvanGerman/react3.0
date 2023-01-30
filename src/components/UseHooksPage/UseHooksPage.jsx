2749da84e7513d5474068a8ed818a391dcef9129
IvanGerman <ivgvi@mail.ru>
2023-01-30 17:38:37 +0700

    feat: use useState hook on UseHookPage component

--- src/components/UseHooksPage/UseHooksPage.jsx
+++ src/components/UseHooksPage/UseHooksPage.jsx
@@ -1,15 +1,37 @@
 import React from "react";
+import { useState } from "react";
 
 import styles from './UseHooksPage.module.css';
 
 
 const UseHooksPage = (props) => { 
 
+  //this 3 lines are explaining the line below
+  //let localState = useState(true);
+  //let editMode = localState[0];
+  //let setEditMode = localState[1];
+  let [editMode, setEditMode] = useState(false);
+  let [value1, setValue1] = useState(1);
+
+  const activateEditMode = () => {
+    setEditMode(true);
+    console.log('editModeAct---',editMode);
+    setValue1(value1+=1);
+    console.log('value1---',value1);
+  };
+
+  const deactivateEditMode = () => {
+    setEditMode(false);
+    console.log('editModeDeact---',editMode);
+  }
 
   return (
     <div className={styles.useHooksPage}>
       
       <p>UseHooksPage</p>
+      <span onDoubleClick = { activateEditMode } >status</span>
+      <input type="text" autoFocus={true} onBlur = { deactivateEditMode } />
+      <p> { value1 } </p>
       
     </div>
   )
